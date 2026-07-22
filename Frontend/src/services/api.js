76bf05eb854110e1fed7647aca64425bccd51788
Vetri170604 <vetrikumar6380268095@gmail.com>
2026-07-22 20:48:15 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const API = axios.create({
 
     baseURL:
-        "http://127.0.0.1:5000/api"
+        "https://employee-attendance-management-system-ayem.onrender.com"
 
 });
 API.interceptors.request.use(
