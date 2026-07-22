a3dcf5cd880428c795bad55ff72ab189939e6c60
Vetri170604 <vetrikumar6380268095@gmail.com>
2026-07-22 21:37:02 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,33 @@ import axios from "axios";
 const API = axios.create({
 
     baseURL:
-        "https://employee-attendance-management-system-ayem.onrender.com"
+        import axios from "axios";
+
+const API = axios.create({
+    baseURL:
+        "https://employee-attendance-management-system-ayem.onrender.com/api"
+});
+
+API.interceptors.request.use(
+    (config) => {
+
+        const token =
+            localStorage.getItem("access_token");
+
+        if (token) {
+            config.headers.Authorization =
+                `Bearer ${token}`;
+        }
+
+        return config;
+    },
+
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default API;
 
 });
 API.interceptors.request.use(
