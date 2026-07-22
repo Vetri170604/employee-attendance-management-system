7005c148fc794b64c2a780ec64d287df311a13bc
Vetri170604 <vetrikumar6380268095@gmail.com>
2026-07-22 21:38:26 +0530

    Refactor API setup and remove duplicate code

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,67 +1,22 @@
 import axios from "axios";
-const API = axios.create({
-
-    baseURL:
-        import axios from "axios";
 
 const API = axios.create({
-    baseURL:
-        "https://employee-attendance-management-system-ayem.onrender.com/api"
+    baseURL: "https://employee-attendance-management-system-ayem.onrender.com/api"
 });
 
 API.interceptors.request.use(
     (config) => {
-
-        const token =
-            localStorage.getItem("access_token");
+        const token = localStorage.getItem("access_token");
 
         if (token) {
-            config.headers.Authorization =
-                `Bearer ${token}`;
+            config.headers.Authorization = `Bearer ${token}`;
         }
 
         return config;
     },
-
     (error) => {
         return Promise.reject(error);
     }
 );
-
-export default API;
-
-});
-API.interceptors.request.use(
-
-    (config) => {
-
-        const token =
-            localStorage.getItem(
-                "access_token"
-            );
-
-
-        if (token) {
-
-            config.headers.Authorization =
-                `Bearer ${token}`;
-
-        }
-
-
-        return config;
-
-    },
-
-    (error) => {
-
-        return Promise.reject(
-            error
-        );
-
-    }
-
-);
-
 
 export default API;
